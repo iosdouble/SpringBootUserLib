81db52589ee5553f6c9985e70339f7bab1c3c667
nihui <18202504057@163.com>
2019-04-21 19:03:34 +0800

    thymeleaf

---
+++ thymeleaf/src/main/java/com/example/thymeleaf/bean/Student.java
@@ -0,0 +1,56 @@
+package com.example.thymeleaf.bean;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @ClassName Strudent
+ * @Author nihui
+ * @Date 2019/4/21 18:27
+ * @Version 1.0
+ * @Description TODO
+ */
+public class Student {
+    private Integer sid;
+    private String sName;
+    private Integer sAge;
+    private String sSex;
+
+    public Student(Integer sid, String sName, Integer sAge, String sSex) {
+        this.sid = sid;
+        this.sName = sName;
+        this.sAge = sAge;
+        this.sSex = sSex;
+    }
+
+    public Integer getSid() {
+        return sid;
+    }
+
+    public void setSid(Integer sid) {
+        this.sid = sid;
+    }
+
+    public String getsName() {
+        return sName;
+    }
+
+    public void setsName(String sName) {
+        this.sName = sName;
+    }
+
+    public Integer getsAge() {
+        return sAge;
+    }
+
+    public void setsAge(Integer sAge) {
+        this.sAge = sAge;
+    }
+
+    public String getsSex() {
+        return sSex;
+    }
+
+    public void setsSex(String sSex) {
+        this.sSex = sSex;
+    }
+}
---
+++ thymeleaf/src/main/java/com/example/thymeleaf/controller/ThymeleafController.java
@@ -0,0 +1,23 @@
+package com.example.thymeleaf.controller;
+
+import com.example.thymeleaf.bean.Student;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @ClassName ThymeleafController
+ * @Author nihui
+ * @Date 2019/4/21 18:26
+ * @Version 1.0
+ * @Description TODO
+ */
+@Controller
+@RequestMapping(value = "/student")
+public class ThymeleafController {
+
+
+}

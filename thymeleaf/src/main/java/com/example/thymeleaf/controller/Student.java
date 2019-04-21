42a1fe9f0d7d512450441b5a3451501a05351b92
nihui <18202504057@163.com>
2019-04-21 19:06:31 +0800

    thymeleaf

---
+++ thymeleaf/src/main/java/com/example/thymeleaf/controller/Student.java
@@ -0,0 +1,11 @@
+package com.example.thymeleaf.controller;
+
+/**
+ * @ClassName Student
+ * @Author nihui
+ * @Date 2019/4/21 19:05
+ * @Version 1.0
+ * @Description TODO
+ */
+public class Student {
+}
--- thymeleaf/src/main/java/com/example/thymeleaf/controller/ThymeleafController.java
+++ thymeleaf/src/main/java/com/example/thymeleaf/controller/ThymeleafController.java
@@ -1,6 +1,5 @@
 package com.example.thymeleaf.controller;
 
-import com.example.thymeleaf.bean.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,6 +17,15 @@ import java.util.List;
 @Controller
 @RequestMapping(value = "/student")
 public class ThymeleafController {
-
-
+   @RequestMapping(value = "/")
+    public ModelAndView index(){
+        List<Student> studentList = new ArrayList<>();
+        studentList.add(new Student(1,"倪辉",24,"男"));
+        studentList.add(new Student(2,"张无忌",18,"男"));
+        studentList.add(new Student(3,"赵敏",16,"女"));
+        studentList.add(new Student(4,"杨过",25,"男"));
+        ModelAndView modelAndView = new ModelAndView("/index");
+        modelAndView.addObject("studentList",studentList);
+        return  modelAndView;
+    }
 }
